02c80b781f02ac32b1cef964928a17aaa86ca9f8
yashwanth_git_test <yashwanth3445@gmail.com>
2025-08-17 00:05:12 -0700

    made a stash

--- gittest2.py
+++ gittest2.py
@@ -1,1 +1,7 @@
 print('new folder with 2nd commit##Got the bug yeyyee')
+#stach
+print("We will try to stach this part of file")
+print("following are the line that I was workings on")
+print("additional extesion_1")
+print("additional extesion_2")
+print('Now Iguesss BUG showed up in the 1st line need to fix it ')
