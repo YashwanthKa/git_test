379c30e9d0bbd8290c18f979063c2bd6c6531591
yashwanth_git_test <yashwanth3445@gmail.com>
2025-08-16 22:27:32 -0700

    erro resloved might be commit 6-7 made changes in the error

--- gittest.py
+++ gittest.py
@@ -1,8 +1,8 @@
 print("hello, I guess this is my 1st commit")
 
-print("just created a new branch_1")
+print("just created a new ## branch_2 ##")
 print("Assume that there are the lines that are about to get the changes")
-print("Lets go with error one")
+print("ERROR ONE RESLOVED")
 print("this might be error for the merge_1")
 print("this might be error for the merge_2")
 print("this might be error for the merge_3")
